4fe629b293a8c6b27323637029d895098d38acb8
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-07-28 22:55:01 +0530

    fetch online API from Newsapi.org

--- src/components/News.js
+++ src/components/News.js
@@ -53,6 +53,57 @@ export default class News extends Component {
       content:
         "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
     },
+    {
+      source: {
+        id: "the-irish-times",
+        name: "The Irish Times",
+      },
+      author: "Kurt Wagner",
+      title:
+        "Twitter to pare back Dublin office space as part of global cost-cutting plan",
+      description:
+        "No jobs will be lost as a result of the move, the company told employees in an email sent on Wednesday.",
+      url: "https://www.irishtimes.com/business/2022/07/28/twitter-to-pare-back-dublin-office-space-as-part-of-global-cost-cutting-plan/",
+      urlToImage:
+        "https://www.irishtimes.com/resizer/_lqLIems8yqwK9pZL6kBt_2tck0=/1200x630/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/irishtimes/HDO6JOBDLX2YTVM2FBG6WHJW4M.jpg",
+      publishedAt: "2022-07-28T09:02:02Z",
+      content:
+        "Twitter is planning to scale back its Dublin office space as part of a global push to cut costs.\r\nThe social media company will reduce the size of its European headquarters at 1 Cumberland Street in … [+2066 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Gadgets360.com",
+      },
+      author: "Reuters",
+      title:
+        "Rivian Plans to Cut Headcount by 6 Percent to Optimise Costs: All Details",
+      description:
+        "Rivian will cut its headcount by 6 percent, the electric truck maker said on Wednesday. The company will focus on enhancing R1 and EDV (electric delivery van) and speed up the development and launch of R2 and future platforms over the next 18 months, and told…",
+      url: "https://gadgets360.com/transportation/news/rivian-electric-truck-maker-cut-jobs-6-percent-optimising-costs-economic-situation-3201695",
+      urlToImage:
+        "https://i.gadgets360cdn.com/large/rivian_image_reuters_1636176975505.jpg",
+      publishedAt: "2022-07-28T08:43:30Z",
+      content:
+        "Electric-truck maker Rivian Automotive said on Wednesday it was reducing headcount by 6 percent in order to optimise costs in a tightening macroeconomic environment.\r\nStaff in the manufacturing opera… [+1756 chars]",
+    },
+    {
+      source: {
+        id: null,
+        name: "Business Standard",
+      },
+      author: "IANS",
+      title:
+        "Tesla's Autopilot system kills another motorcyclist, US begins probe",
+      description:
+        "A Tesla vehicle has allegedly killed another motorcyclist, this time in the Utah state in the US when a driver using Autopilot slammed into the rear of his bike.",
+      url: "https://www.business-standard.com/article/technology/tesla-s-autopilot-system-kills-another-motorcyclist-us-begins-probe-122072800511_1.html",
+      urlToImage:
+        "https://bsmedia.business-standard.com/_media/bs/img/article/2022-06/23/full/1655951853-0959.jpg",
+      publishedAt: "2022-07-28T08:39:00Z",
+      content:
+        "A Tesla vehicle has allegedly killed another motorcyclist, this time in the Utah state in the US when a driver using Autopilot slammed into the rear of his bike.\r\nThe National Highway Traffic Safety … [+2798 chars]",
+    },
   ];
   constructor() {
     super();
@@ -63,20 +114,29 @@ export default class News extends Component {
     };
   }
 
+  async componentDidMount(){
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=b06f36dd8264465b8ae9bd173a3ac15e"
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    console.log(parsedData);
+    this.setState({articles:parsedData.articles})
+  }
+
   render() {
     return (
       <div className="container my-3">
-        <h2>Todys!Updates :- Headlines of the day! </h2>
+        <h1>Todys!Updates :- Headlines of the day! </h1>
         {/* {this.state.articles.map((element)=>{console.log(element);})} */}
         <div className="row">
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.urlToImage}>
                 <NewsItems
-                  title={element.title.slice(0, 50)}
-                  discription={element.description.slice(0, 88)}
-                  imageUrl={element.urlToImage}
-                  newsUrl ={element.url}
+                  title={element.title?element.title.slice(0, 45):""}
+                  discription={element.description?element.description.slice(0,80):""}
+                  imageUrl={element.urlToImage?element.urlToImage:"https://images.hindustantimes.com/tech/img/2022/07/28/1600x900/jpegPIA25017_(1)_1658994714280_1659008302741_1659008302741.jpg"}
+                  newsUrl={element.url}
                 />
               </div>
             );
