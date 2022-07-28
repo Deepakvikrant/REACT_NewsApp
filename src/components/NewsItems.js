6b4f115896c7146ead558dbb623c6f51492950a6
Deepakvikrant <100572841+Deepakvikrant@users.noreply.github.com>
2022-07-28 14:40:19 +0530

    news added to view

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 
 export default class NewsItems extends Component {
-
-
   render() {
-    let {title,discription,imageUrl} = this.props
-
+    let { title, discription, imageUrl, newsUrl } = this.props;
 
     return (
       <div className="container my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{ width: "18rem" }}>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">
-             {discription}
-            </p>
-            <a href="/newsDetails" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{discription}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+              rel="noreferrer"
+            >
               Read more
             </a>
           </div>
